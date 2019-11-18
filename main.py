cfeb7aa3deff3c1d14c0c95c0d42473db19d8a3e
albertobazilio <albertobazilio@jera.com.br>
2019-11-18 13:33:49 -0400

    [NEW] brand window

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ from collections import OrderedDict
 import view.Estoque as estoque
 import view.Produtos as produtos
 import view.Clientes as clientes
+import view.Marcas as marcas
 
 main = Tk()
 
 mostrarEstoqueBtn = Button(main, text="Mostrar Estoque", command=estoque.mostrar)
 mostrarProdutosBtn = Button(main, text="Mostrar Produtos", command=produtos.mostrar)
 mostrarClientesBtn = Button(main, text="Mostrar Clientes", command=clientes.mostrar)
+mostrarMarcasBtn = Button(main, text="Mostrar Marcas", command=marcas.mostrar)
 mostrarEstoqueBtn.pack()
 mostrarProdutosBtn.pack()
 mostrarClientesBtn.pack()
+mostrarMarcasBtn.pack()
 
 main.mainloop()
---
+++ view/Marcas.py
@@ -0,0 +1,67 @@
+from tkintertable import TableCanvas, TableModel
+from tkinter import *
+from collections import OrderedDict
+
+#lib que gera dados aleatórios remover quando tiver os dados do banco
+from tkintertable.Testing import sampledata
+import random
+
+
+data = sampledata()
+
+def mostrar():
+  # funcao que chama a janela de marca
+  mostrarWindow = Toplevel()
+
+  # Frame de busca
+  def buscar():
+    print("batata")
+  
+  buscaFrame = Frame(mostrarWindow)
+  buscaInput = Entry(buscaFrame)
+  buscaBtn = Button(buscaFrame, text="Buscar", command=buscar)
+  buscaInput.pack(side=LEFT)
+  buscaBtn.pack(side=LEFT)
+
+  buscaFrame.pack()
+
+  # Frame da tabela de marca
+  marcaFrame = Frame(mostrarWindow)
+  tabelaMarca = TableCanvas(marcaFrame, data=data)
+  tabelaMarca.show()
+  marcaFrame.pack()
+
+  # Frame de actions
+  actionsFrame = Frame(mostrarWindow)
+  cadastrarBtn = Button(actionsFrame, text="Cadastrar Novo", command=cadastrar)
+
+  cadastrarBtn.pack(side=LEFT)
+  actionsFrame.pack()
+
+  mostrarWindow.mainloop()
+
+def cadastrar():
+  # Funcao que chama janela de cadastar novo produto
+  cadastrarWindow = Toplevel()
+
+  # Frame do formulario de produto
+  cadastrarForm = Frame(cadastrarWindow)
+  nomeLabel = Label(cadastrarForm, text="Nome da Marca")
+  nomeInput = Entry(cadastrarForm)
+
+  nomeLabel.grid(row=0, column=0)
+  nomeInput.grid(row=0, column=1)
+
+  cadastrarForm.pack()
+
+  # Frame de acoes do formulario
+  def cadastrarProduto():
+    print(nomeInput.get())
+  produtoMarcaActions = Frame(cadastrarWindow)
+  cadastrarProdutoBtn = Button(produtoMarcaActions, text="Cadastar", command=cadastrarProduto)
+
+  cadastrarProdutoBtn.pack(side=LEFT)
+  produtoMarcaActions.pack()
+
+
+  cadastrarWindow.mainloop()
